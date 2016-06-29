b0fa9a5bc80ff279429d0545eee7cc3bbcf64877
Christopher A <chris.altig@gmail.com>
2016-06-28 23:06:10 -0700

    Added GUI + Options + Life/Immigration/Rainbow Life

--- GameOfLifeScreensaver/GameOfLifeScreensaver/life.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-double getRandomFloatInRange(double min, double max);
-char* createSimulation(int rows, int columns, int boundary, double density);
-int countNeighborhood(int rows, int columns, int x, int y, char* matrix);
-char* stepSimulation(int rows, int columns, int boundary, char* prevMatrix); 
-void applyBorders(int rows, int columns, char* matrix);
-void convertSimulation(int* screen, int screenWidth, int color, char* simulation, int rows, int columns, int cellSize);
-void specialPlot(int* screen, int screenWidth, int x, int y, int size, int color);
--- GameOfLifeScreensaver/GameOfLifeScreensaver/math_custom.h
+++
@@ -1,7 +0,0 @@
-#define M_PI 3.141592653589793238462643383279502884197169399375105820974
-
-int Greatest(int num, int num2);
-int Least(int num, int num2);
-int Difference(int num, int num2);
-int Abs(int val);
-int GetColor(double* angle);
